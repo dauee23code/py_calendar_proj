874638e16c9dfd893de31b9286f2b7ee9a77c4a2
user <dauee23@donga.ac.kr>
2023-11-19 06:25:28 +0900

    code1

--- py_calendar_proj/git/getVilageFcst.py
+++ py_calendar_proj/git/getVilageFcst.py
@@ -1,11 +1,15 @@
 import requests
+import json
+import time
+from urllib.request import urlopen
 from datetime import datetime
-from bs4 import BeautifulSoup
+
 
 # 날짜(YYYYMMDD) 출력 함수
 # 오늘의 날짜를 YYYYMMDD 형태로 반환한다.
 def base_date():
     return datetime.today().strftime('%Y%m%d')
+
 
 # 시각(HH00) 출력 함수
 # 기상청 단기예보 발표 시각 중 현재 시각과 비교하여 최신의 시각을 반환한다.
@@ -16,7 +20,7 @@ def base_time():
 
     # 기상청 단기예보 발표 시각 중 현재 시각과 가까운 값을 구한다.
     announcement.sort()
-    N = min(announcement, key=lambda x: abs(x - n))
+    N = min(announcement, key=lambda var: abs(var - n))
 
     # 값이 현재 시각보다 크면 그 이전의 시각을 구한다.
     if N <= n:
@@ -33,42 +37,27 @@ def base_time():
     else:
         return f'0{x}00'
 
+
 # [A시 B구 C동] -> [기상청 격자정보 (nx, ny)] 치환 함수
 # city, mdl, leaf 변수 선언이 선행되어야 (nx, ny) 값을 반환한다.
 def get_nxy():
-    response1 = requests.get(f'https://www.kma.go.kr/DFSROOT/POINT/DATA/top.json.txt')
-    # 서버가 GET 요청을 제대로 처리했는가? [code == 200] -> [성공]
-    if response1.status_code == 200:
-        response1.encoding = 'utf-8'
-        # eval 함수로 문자열을 형변환하고, list 형태로 변수에 저장
-        top_list = eval(str(BeautifulSoup(response1.text, 'html.parser')))
-    else:
-        # [code != 200] -> [서버의 상태 코드를 출력]
-        print(response1.status_code)
+    global nx, ny
 
-    for _ in range(len(top_list)):
-        if city in top_list[_].values():
-            city_code = str(top_list[_].get('code'))
-            break
+    with urlopen(f'https://www.kma.go.kr/DFSROOT/POINT/DATA/top.json.txt') as resp:
+        response1 = json.load(resp)
+        top_list = eval(str(response1))
 
-    response2 = requests.get(f'https://www.kma.go.kr/DFSROOT/POINT/DATA/mdl.{city_code}.json.txt')
-    if response2.status_code == 200:
-        response2.encoding = 'utf-8'
-        mdl_list = eval(str(BeautifulSoup(response2.text, 'html.parser')))
-    else:
-        print(response2.status_code)
+    city_code = [str(top_list[_].get('code')) for _ in range(len(top_list)) if city in top_list[_].values()][0]
 
-    for _ in range(len(mdl_list)):
-        if mdl in mdl_list[_].values():
-            mdl_code = str(mdl_list[_].get('code'))
-            break
+    with urlopen(f'https://www.kma.go.kr/DFSROOT/POINT/DATA/mdl.{str(city_code)}.json.txt') as resp:
+        response2 = json.load(resp)
+        mdl_list = eval(str(response2))
 
-    response3 = requests.get(f'https://www.kma.go.kr/DFSROOT/POINT/DATA/leaf.{mdl_code}.json.txt')
-    if response3.status_code == 200:
-        response3.encoding = 'utf-8'
-        leaf_list = eval(str(BeautifulSoup(response3.text, 'html.parser')))
-    else:
-        print(response3.status_code)
+    mdl_code = [str(mdl_list[_].get('code')) for _ in range(len(mdl_list)) if mdl in mdl_list[_].values()][0]
+
+    with urlopen(f'https://www.kma.go.kr/DFSROOT/POINT/DATA/leaf.{str(mdl_code)}.json.txt') as resp:
+        response3 = json.load(resp)
+        leaf_list = eval(str(response3))
 
     for _ in range(len(leaf_list)):
         if leaf in leaf_list[_].values():
@@ -78,12 +67,15 @@ def get_nxy():
 
     return nx, ny
 
-# 기상청 단기예보(json data) 출력 함수
-# (nx, ny)의 변수 선언이 선행되어야 한다.
+
+# 기상청 단기예보 출력 함수
+# nx, ny 변수 선언이 선행되어야 json 데이터를 반환한다.
 # 공공데이터포털(data.go.kr)에서 일반 인증키(Encoding) 발급 필요
-def getVilageFcst():
+def get_VF():
     url = 'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst'
-    params = {'serviceKey': '### 일반 인증키 (Encoding) ###',
+    serviceKey = '### 일반 인증키 (Encoding) ###'
+    time.sleep(0.5)
+    params = {'serviceKey': {serviceKey},
               'pageNo': '1',
               'numOfRows': '1000',
               'dataType': 'json',
@@ -95,14 +87,22 @@ def getVilageFcst():
     response = requests.get(url, params=params)
     return response.content
 
-if __name__ == "__main__":
-    # get_nxy 함수를 사용하기 위해서는 city, mdl, leaf 변수가 선언되어야 한다.
-    city, mdl, leaf = input('INSERT YOUR ADDRESS (ex. 부산광역시 사하구 하단2동) : ').split()
 
-    # 기상청의 격자정보 (nx, ny)를 각각의 변수에 저장하고, 출력한다.
-    nx, ny = get_nxy()
-    print(f'YOUR GRID INFO : ({nx}, {ny})\n')
+if __name__ == "__main__":
+    # nx, ny의 값을 사용자로부터 받아온다.
+    temp = str(input('INSERT YOUR GRID INFO (ex. 96 74) OR ENTER : '))
+    if temp != '':
+        nx, ny = temp.split()
+    del temp
+
+    if ('nx' and 'ny') not in locals():
+        # get_nxy 함수를 사용하기 위해서는 city, mdl, leaf 변수가 선언되어야 한다.
+        city, mdl, leaf = input('INSERT YOUR ADDRESS (ex. 부산광역시 사하구 하단2동) : ').split()
+
+        # get_nxy 함수를 사용하여 기상청의 격자정보 (nx, ny)를 출력한다.
+        nx, ny = get_nxy()
+        print(f'YOUR GRID INFO : ({nx}, {ny})\n')
 
     # 입력된 정보를 통해 기상청에 데이터를 요청하고, 출력한다.
-    data = getVilageFcst()
+    data = get_VF()
     print(data)
