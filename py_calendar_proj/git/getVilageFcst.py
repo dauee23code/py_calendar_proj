680c9ae7a032657f33c02eca0e3672395a2d90b0
user <dauee23@donga.ac.kr>
2023-11-22 23:41:41 +0900

    code1

--- py_calendar_proj/git/getVilageFcst.py
+++ py_calendar_proj/git/getVilageFcst.py
@@ -1,92 +1,127 @@
-import requests
-import json
-import time
-from urllib.request import urlopen
+import ast
 from datetime import datetime
+import requests
+from urllib import parse
 
 
-# 날짜(YYYYMMDD) 출력 함수
-# 오늘의 날짜를 YYYYMMDD 형태로 반환한다.
-def base_date():
-    return datetime.today().strftime('%Y%m%d')
+# [날짜(YYYYMMDD), 시각(HH00)] 출력 함수
+def get_base():
+    global base_date, base_time
 
+    base_date = datetime.today().strftime('%Y%m%d')
 
-# 시각(HH00) 출력 함수
-# 기상청 단기예보 발표 시각 중 현재 시각과 비교하여 최신의 시각을 반환한다.
-def base_time():
-    # 현재 시각은 정수로, 기상청 단기예보 발표 시각은 리스트로 각각 변수에 저장한다.
+    # 현재 시각은 정수 형태로, 기상청 단기예보 발표 시각은 리스트 형태로 변수 n, Ann 을 선언한다.
     n = int(str(datetime.now().hour))
-    announcement = [2, 5, 8, 11, 14, 17, 20, 23]
+    Ann = [2, 5, 8, 11, 14, 17, 20, 23]
 
     # 기상청 단기예보 발표 시각 중 현재 시각과 가까운 값을 구한다.
-    announcement.sort()
-    N = min(announcement, key=lambda var: abs(var - n))
+    Ann.sort()
+    N = min(Ann, key=lambda var: abs(var - n))
 
-    # 값이 현재 시각보다 크면 그 이전의 시각을 구한다.
-    if N <= n:
+    # 값이 현재 시각보다 크면 그 이전의 시각을 구하고, 날짜가 어제가 될 경우 base_date 에 1을 뺀 값을 넣는다.
+    if N < n or (N == n and datetime.now().minute > 10):
         x = N
     else:
         if N - 3 < 0:
-            x = announcement[-1]
+            x = Ann[-1]
+            base_date = str(ast.literal_eval(base_date)-1)
         else:
-            x = N - 3
+            x = N-3
 
-    # 변수 x의 값을 HH00 형태로 치환한다.
     if x >= 10:
-        return f'{x}00'
+        base_time = f'{x}00'
     else:
-        return f'0{x}00'
+        base_time = f'0{x}00'
+
+    return base_date, base_time
 
 
 # [A시 B구 C동] -> [기상청 격자정보 (nx, ny)] 치환 함수
-# city, mdl, leaf 변수 선언이 선행되어야 (nx, ny) 값을 반환한다.
+# 다음의 변수 선언이 선행되어야 한다. [city, mdl, leaf]
 def get_nxy():
     global nx, ny
 
-    with urlopen(f'https://www.kma.go.kr/DFSROOT/POINT/DATA/top.json.txt') as resp:
-        response1 = json.load(resp)
+    # 변수 city, mdl, leaf 값을 value 값으로 가지는 딕셔너리를 찾고, key 값이 "code"인 value 값을 가져온다.
+    response1 = ast.literal_eval(
+        requests.get(f'https://www.kma.go.kr/DFSROOT/POINT/DATA/top.json.txt').content.decode('utf-8'))
+    city_code = [str(response1[i].get('code')) for i in range(len(response1)) if city in response1[i].values()][0]
 
-    # json.load()로 받아온 리스트 안에서 변수 city에 저장된 값이 value 값과 일치하는 딕셔너리를 찾는다.
-    # 찾은 딕셔너리에서 "code"가 key 값인 value 값을 가져온다.
-    city_code = [str(response1[_].get('code')) for _ in range(len(response1)) if city in response1[_].values()][0]
+    response2 = ast.literal_eval(
+        requests.get(f'https://www.kma.go.kr/DFSROOT/POINT/DATA/mdl.{city_code}.json.txt').content.decode('utf-8'))
+    mdl_code = [str(response2[j].get('code')) for j in range(len(response2)) if mdl in response2[j].values()][0]
 
-    with urlopen(f'https://www.kma.go.kr/DFSROOT/POINT/DATA/mdl.{str(city_code)}.json.txt') as resp:
-        response2 = json.load(resp)
+    response3 = ast.literal_eval(
+        requests.get(f'https://www.kma.go.kr/DFSROOT/POINT/DATA/leaf.{mdl_code}.json.txt').content.decode('utf-8'))
 
-    mdl_code = [str(response2[_].get('code')) for _ in range(len(response2)) if mdl in response2[_].values()][0]
-
-    with urlopen(f'https://www.kma.go.kr/DFSROOT/POINT/DATA/leaf.{str(mdl_code)}.json.txt') as resp:
-        response3 = json.load(resp)
-
-    for _ in range(len(response3)):
-        if leaf in response3[_].values():
-            nx = str(response3[_].get('x'))
-            ny = str(response3[_].get('y'))
+    for k in range(len(response3)):
+        if leaf in response3[k].values():
+            nx = str(response3[k].get('x'))
+            ny = str(response3[k].get('y'))
             break
 
     return nx, ny
 
 
-# 기상청 단기예보 출력 함수
-# nx, ny 변수 선언이 선행되어야 json 데이터를 반환한다.
-# 공공데이터포털(data.go.kr)에서 일반 인증키(Encoding) 발급 필요
+# 기상청 단기예보(dataType==json, type==dict) 출력 함수
+# 다음의 변수 선언이 선행되어야 한다. [base_date, base_time, nx, ny]
 def get_VF():
     url = 'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst'
-    params = {'serviceKey': '### 일반 인증키 (Encoding) ###',
-              'pageNo': '1',
-              'numOfRows': '1000',
-              'dataType': 'json',
-              'base_date': base_date(),
-              'base_time': base_time(),
-              'nx': nx,
-              'ny': ny}
+    ServiceKey = '### 일반 인증키 (Encoding) ###'
 
-    response = requests.get(url, params=params)
-    return response.content
+    params = (f'?{parse.quote_plus("ServiceKey")}={ServiceKey}&' +
+              parse.urlencode({
+                  parse.quote_plus('pageNo'): '1',
+                  parse.quote_plus('numOfRows'): '1000',
+                  parse.quote_plus('dataType'): 'json',
+                  parse.quote_plus('base_date'): base_date,
+                  parse.quote_plus('base_time'): base_time,
+                  parse.quote_plus('nx'): nx,
+                  parse.quote_plus('ny'): ny
+              }))
+
+    response = requests.get(url + params, verify=False).json().get('response').get('body').get('items').get('item')
+
+    return response
+
+
+# 강수가 일어날 시간대와 강수확률을 출력한다.
+# val 값이 0이면 오늘, 1이면 내일, 2이면 모레의 값을 나타낸다.
+def rainfall(response, val):
+    POP = []
+
+    if val == 0:
+        for i in response:
+            if i['category'] == 'POP' and i['fcstDate'] == base_date:
+                if i['fcstValue'] != '0':
+                    POP.append(i)
+        print('오늘 강수가 일어날 시간대는 아래와 같습니다.')
+        for j in POP:
+            print(f"{j['fcstTime']} : 강수확률은 {j['fcstValue']}%")
+
+    elif val == 1:
+        for i in response:
+            if i['category'] == 'POP' and i['fcstDate'] == str(ast.literal_eval(base_date)+1):
+                if i['fcstValue'] != '0':
+                    POP.append(i)
+        print('내일 강수가 일어날 시간대는 아래와 같습니다.')
+        for j in POP:
+            print(f"{j['fcstTime']} : 강수확률은 {j['fcstValue']}%")
+
+    elif val == 2:
+        for i in response:
+            if i['category'] == 'POP' and i['fcstDate'] == str(ast.literal_eval(base_date)+2):
+                if i['fcstValue'] != '0':
+                    POP.append(i)
+        print('모레 강수가 일어날 시간대는 아래와 같습니다.')
+        for j in POP:
+            print(f"{j['fcstTime']} : 강수확률은 {j['fcstValue']}%")
+
+    print()
 
 
 if __name__ == "__main__":
-    # nx, ny의 값을 사용자로부터 받아온다.
+    base_date, base_time = get_base()
+
     temp = str(input('INSERT YOUR GRID INFO (ex. 96 74) OR ENTER : '))
     if temp != '':
         nx, ny = temp.split()
@@ -94,18 +129,11 @@ if __name__ == "__main__":
     del temp
 
     if ('nx' and 'ny') not in locals():
-        # get_nxy 함수를 사용하기 위해서는 city, mdl, leaf 변수가 선언되어야 한다.
         city, mdl, leaf = input('INSERT YOUR ADDRESS (ex. 부산광역시 사하구 하단2동) : ').split()
-
-        # get_nxy 함수를 사용하여 기상청의 격자정보 (nx, ny)를 출력한다.
         nx, ny = get_nxy()
         print(f'YOUR GRID INFO : ({nx}, {ny})\n')
 
-    print()
-
-    # 에러 메세지가 아닌 값을 받아올 때까지 3초를 주기로 data.go.kr에 데이터를 요청한다.
-    data = 'errMsg'
-    while 'errMsg' in str(data):
-        data = get_VF()
-        time.sleep(3)
-    print(data)
+    res = get_VF()
+    rainfall(res, 0)
+    rainfall(res, 1)
+    rainfall(res, 2)
